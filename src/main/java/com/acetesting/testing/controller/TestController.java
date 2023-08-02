558a1620bb8a7c7794d7db0babba41bb69ae2603
the25thbaam4 <the25thbaam@tutanota.com>
2023-08-02 10:45:31 +0200

    Initial commit

---
+++ src/main/java/com/acetesting/testing/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.acetesting.testing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+@GetMapping(value = "/test")
+    public String user(){
+    return "weeee";
+}
+}
